a25bd03a94728c370d6f1fabffedbd23ec1a23c3
Timon Arendt <t.arendt@stic-online.org>
2015-03-14 21:32:29 +0100

    use Shield or Armor

--- lua/autorun/bl2_hud.lua
+++ lua/autorun/bl2_hud.lua
@@ -17,32 +17,33 @@ local s_alpha2 = 0
 local sound_1 = false
 
 hook.Add( "HUDPaint", "BL2_Render3", function()
-s_alpha1 = Lerp(1*FrameTime(),s_alpha1,LocalPlayer():Armor())
-surface.SetDrawColor(0,155,255,155 + (LocalPlayer():Armor() - s_alpha1) * 20)
-surface.SetMaterial(Material("sprites/mechshield"))
-surface.DrawTexturedRectUV(0,ScrH() * .97,ScrW(),ScreenScale(70),2 + (CurTime()/10),0 ,0 + (CurTime()/10),.2)
-if LocalPlayer():Armor() > 0 then
-surface.SetDrawColor(0,155,255,0 - (LocalPlayer():Armor() - s_alpha1) * 10)
-surface.SetMaterial(Material("sprites/mechshield"))
-surface.DrawTexturedRect(ScrW() * -.15,ScrH() * -.35,ScreenScale(200),ScreenScale(200))
-surface.DrawTexturedRect(ScrW() * .85,ScrH() * -.35,ScreenScale(200),ScreenScale(200))
-surface.DrawTexturedRect(ScrW() * -.15,ScrH() * .85,ScreenScale(200),ScreenScale(200))
-surface.DrawTexturedRect(ScrW() * .85,ScrH() * .85,ScreenScale(200),ScreenScale(200))
+	local Armor = LocalPlayer():GetNWInt("Shield",LocalPlayer():Armor())
+	s_alpha1 = Lerp(1*FrameTime(),s_alpha1,Armor)
+	surface.SetDrawColor(0,155,255,155 + (Armor - s_alpha1) * 20)
+	surface.SetMaterial(Material("sprites/mechshield"))
+	surface.DrawTexturedRectUV(0,ScrH() * .97,ScrW(),ScreenScale(70),2 + (CurTime()/10),0 ,0 + (CurTime()/10),.2)
+	if Armor > 0 then
+		surface.SetDrawColor(0,155,255,0 - (Armor - s_alpha1) * 10)
+		surface.SetMaterial(Material("sprites/mechshield"))
+		surface.DrawTexturedRect(ScrW() * -.15,ScrH() * -.35,ScreenScale(200),ScreenScale(200))
+		surface.DrawTexturedRect(ScrW() * .85,ScrH() * -.35,ScreenScale(200),ScreenScale(200))
+		surface.DrawTexturedRect(ScrW() * -.15,ScrH() * .85,ScreenScale(200),ScreenScale(200))
+		surface.DrawTexturedRect(ScrW() * .85,ScrH() * .85,ScreenScale(200),ScreenScale(200))
 
-end
-if LocalPlayer():Armor() <= 0 then
-if sound_1 == true then
-util.ScreenShake( EyePos(), 3, 5, 2, 5000 )
-sound_1 = false
-LocalPlayer():EmitSound("buttons/og_button_down_0"..math.random(1,3)..".wav",100,100)
-end
-s_alpha2 = s_alpha2 + 2
-surface.SetDrawColor(0,155,255,155 - (s_alpha2))
-surface.SetMaterial(Material("combinemechhud/broken"))
-surface.DrawTexturedRect(0,0,ScrW(),ScrH())
-else sound_1 = true 
-s_alpha2 = 0
-end
+	end
+	if Armor <= 0 then
+		if sound_1 == true then
+			util.ScreenShake( EyePos(), 3, 5, 2, 5000 )
+			sound_1 = false
+			LocalPlayer():EmitSound("buttons/og_button_down_0"..math.random(1,3)..".wav",100,100)
+		end
+		s_alpha2 = s_alpha2 + 2
+		surface.SetDrawColor(0,155,255,155 - (s_alpha2))
+		surface.SetMaterial(Material("combinemechhud/broken"))
+		surface.DrawTexturedRect(0,0,ScrW(),ScrH())
+	else sound_1 = true 
+		s_alpha2 = 0
+	end
 end )
 local received = {}
 local tablenum = 0
