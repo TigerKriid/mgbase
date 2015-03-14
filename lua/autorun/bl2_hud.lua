ac71af8551b1acae0ff386f28f8d8f1f8578ee44
Timon Arendt <t.arendt@stic-online.org>
2015-03-14 21:20:49 +0100

    Revert "Revert "Update bl2_hud.lua""
    This reverts commit 1a84d2ad08227fd0cef0e0cc23596da2f7bf0026.

--- lua/autorun/bl2_hud.lua
+++ lua/autorun/bl2_hud.lua
@@ -1,48 +1,62 @@
---fucking finally, 3 hours of this bullshit. -.-
+
 if SERVER then
+util.AddNetworkString("bl2_3d2dshield")
 
-	util.AddNetworkString("bl2_3d2dshield")
-	local sv_sending = {}
-	function SendDamageInfo(attacker,pos,damage,victim)
-		local data =  	{
-							faggot = attacker,
-							faggotpos = attacker:GetPos(),
-							faggotang = attacker:GetAngles(),
-							dmgpos = pos,
-							dmgnumber = damage,
-							alpha = 255,
-							secondfaggot = victim,
-						}
-
-		net.Start("bl2_3d2dshield")
-			net.WriteTable(data)
-		net.Broadcast()
-	end
-	hook.Add("EntityTakeDamage","bl2takedamageplz",function(target,dmg)
-		if target:IsPlayer() and target:Armor() > 0 then
-			SendDamageInfo(dmg:GetAttacker(),dmg:GetDamagePosition(),dmg:GetDamage(),target)
-		end
-	end)
+hook.Add("PlayerHurt","fuckthisshitpolz",function(victim,attacker,damageTaken,healthRemaining)
+if victim:IsPlayer() and victim:Armor() > 0 then
+net.Start("bl2_3d2dshield")
+net.WriteTable({victim,attacker,healthRemaining})
+net.Broadcast()
 end
-
-
-
-
-if CLIENT then
-local received = {}
-net.Receive("bl2_3d2dshield",function(len,ply)
-	received[#received + 1] = net.ReadTable()
 end)
+
+end
+if CLIENT then
+local s_alpha1 = 0
+local s_alpha2 = 0
+local sound_1 = false
+
+hook.Add( "HUDPaint", "BL2_Render3", function()
+s_alpha1 = Lerp(1*FrameTime(),s_alpha1,LocalPlayer():Armor())
+surface.SetDrawColor(0,155,255,155 + (LocalPlayer():Armor() - s_alpha1) * 20)
+surface.SetMaterial(Material("sprites/mechshield"))
+surface.DrawTexturedRectUV(0,ScrH() * .97,ScrW(),ScreenScale(70),2 + (CurTime()/10),0 ,0 + (CurTime()/10),.2)
+if LocalPlayer():Armor() > 0 then
+surface.SetDrawColor(0,155,255,0 - (LocalPlayer():Armor() - s_alpha1) * 10)
+surface.SetMaterial(Material("sprites/mechshield"))
+surface.DrawTexturedRect(ScrW() * -.15,ScrH() * -.35,ScreenScale(200),ScreenScale(200))
+surface.DrawTexturedRect(ScrW() * .85,ScrH() * -.35,ScreenScale(200),ScreenScale(200))
+surface.DrawTexturedRect(ScrW() * -.15,ScrH() * .85,ScreenScale(200),ScreenScale(200))
+surface.DrawTexturedRect(ScrW() * .85,ScrH() * .85,ScreenScale(200),ScreenScale(200))
+
+end
+if LocalPlayer():Armor() <= 0 then
+if sound_1 == true then
+util.ScreenShake( EyePos(), 3, 5, 2, 5000 )
+sound_1 = false
+LocalPlayer():EmitSound("buttons/og_button_down_0"..math.random(1,3)..".wav",100,100)
+end
+s_alpha2 = s_alpha2 + 2
+surface.SetDrawColor(0,155,255,155 - (s_alpha2))
+surface.SetMaterial(Material("combinemechhud/broken"))
+surface.DrawTexturedRect(0,0,ScrW(),ScrH())
+else sound_1 = true 
+s_alpha2 = 0
+end
+end )
+local received = {}
+local tablenum = 0
 
 hook.Add("PostDrawOpaqueRenderables","hllyshit",function()
 	for i,d in pairs(received) do
 		if d.alpha > 1 then
-			d.alpha = Lerp(5*FrameTime(),d.alpha,0)
+			d.alpha = Lerp(1*FrameTime(),d.alpha,0)
 			local offset = Angle(270,d.faggotang.y,0)
-			cam.Start3D2D(d.dmgpos + offset:Up() * 35,Angle(270,d.faggotang.y,0),.5)
+
+			cam.Start3D2D(d.dmgpos + d.randompos + offset:Up() * 15,Angle(270,d.faggotang.y,0),.5)
 				surface.SetDrawColor(0,155,255,d.alpha) 
 				surface.SetMaterial(Material("sprites/mechshield"))
-				local size = d.dmgnumber * 2
+				local size = 20 + d.dmgnumber
 				surface.DrawTexturedRect(-size/2,-size/2,size,size)
 			cam.End3D2D()
 		else
@@ -52,35 +66,17 @@ hook.Add("PostDrawOpaqueRenderables","hllyshit",function()
 	end
 end)
 
-local s_alpha1 = 0
-local s_alpha2 = 0
-local sound_1 = false
-
-hook.Add( "HUDPaint", "BL2_Render3", function()
-s_alpha1 = Lerp(1*FrameTime(),s_alpha1,LocalPlayer():Armor())
-if LocalPlayer():Armor() > 0 then
-surface.SetDrawColor(0,155,255,0 - (LocalPlayer():Armor() - s_alpha1) * 10)
-surface.SetMaterial(Material("sprites/mechshield"))
-surface.DrawTexturedRect(ScrW() * -.15,ScrH() * -.35,ScreenScale(200),ScreenScale(200))
-surface.DrawTexturedRect(ScrW() * .85,ScrH() * -.35,ScreenScale(200),ScreenScale(200))
-surface.DrawTexturedRect(ScrW() * -.15,ScrH() * .85,ScreenScale(200),ScreenScale(200))
-surface.DrawTexturedRect(ScrW() * .85,ScrH() * .85,ScreenScale(200),ScreenScale(200))
-end
-if LocalPlayer():Armor() <= 0 then
-if sound_1 == true then
-util.ScreenShake( EyePos(), 3, 5, 2, 5000 )
-sound_1 = false
-LocalPlayer():EmitSound("plats/crane/vertical_stop.wav",50,100)
-end
-s_alpha2 = s_alpha2 + 2
-surface.SetDrawColor(0,155,255,155 - (s_alpha2))
-surface.SetMaterial(Material("combinemechhud/broken"))
-surface.DrawTexturedRect(0,0,ScrW(),ScrH())
-else sound_1 = true s_alpha2 = 0 end
-end )
-
-hook.Add("PostDrawOpaqueRenderables","BL2_Render4",function()
-
+net.Receive("bl2_3d2dshield",function(len,ply)
+local cltable = net.ReadTable()
+tablenum = tablenum + 1
+received[tablenum] = {		faggot = cltable[2],
+							faggotpos = cltable[2]:GetPos(),
+							faggotang = cltable[2]:GetAngles(),
+							dmgnumber = cltable[3],
+							dmgpos = cltable[1]:EyePos(),
+							randompos = Vector(0,0,math.random(-20,2)),
+							alpha = 255,
+							secondfaggot = cltable[1],
+}
 end)
-
 end
