31cb7028f97a8034cde356802f627c0099f410d4
Mushroomguy <alexiocsgo@gmail.com>
2015-03-16 22:07:44 +0100

    Create polydrawer.lua

---
+++ lua/autorun/polydrawer.lua
@@ -0,0 +1,42 @@
+-- a handy tool to design anything by clicking on your screen.
+-- cursor's x and y are automatically scaled to ScrH() and ScrW(), so you literally have to copy paste what you get :)
+
+if CLIENT then
+local PolyTable = {}
+local PolyTable_Index = 0
+local MousePause = 0
+local cvarenable = CreateClientConVar("tool_drawpoly","0",FCVAR_NONE)
+
+concommand.Add("tool_drawpoly_cleartable",function()
+	PolyTable = {}
+	PolyTable_Index = 0
+	LocalPlayer():EmitSound("items/ammocrate_close.wav")
+end)
+
+concommand.Add("tool_drawpoly_printtable",function()
+PrintTable(PolyTable)
+end)
+
+hook.Add("HUDPaint","TOOL_Poly",function()
+if cvarenable:GetInt() == 1 and vgui.CursorVisible() then
+if LocalPlayer():KeyDown(IN_ATTACK) then 
+	if CurTime() >= MousePause then
+	MousePause = CurTime() + .5
+	PolyTable_Index = PolyTable_Index + 1
+	PolyTable[PolyTable_Index] = {x = math.Truncate(ScrW() /gui.MouseX(),3),y = math.Truncate(ScrH() /gui.MouseY(),3)}
+	LocalPlayer():EmitSound("items/medshot4.wav")
+end
+end
+
+for i, v in pairs(PolyTable) do
+	surface.SetDrawColor(255,0,0,255)
+	surface.SetTextColor(255,0,0,255)
+surface.DrawCircle(ScrW() / v.x,ScrH() / v.y,math.sin(CurTime() * 20 - i) * 2 + i * .5,Color(255,0,0,255))
+if #PolyTable > 1 then
+surface.DrawText(i,"Default",ScrW() / v.x,ScrH() / v.y)
+surface.DrawLine(ScrW() / v.x,ScrH() / v.y,ScrW() / table.FindNext(PolyTable,v).x,ScrH() / table.FindNext(PolyTable,v).y)
+end
+end
+end
+end)
+end
