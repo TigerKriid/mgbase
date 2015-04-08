fa1d6a5ac2a45883b2b644a9cacb3ffba5b45bc5
Timon Arendt <t.arendt@stic-online.org>
2015-04-08 22:59:12 +0200

    morefix

--- lua/autorun/BorderlandsChests.lua
+++ lua/autorun/BorderlandsChests.lua
@@ -132,7 +132,84 @@ hook.Add("Think","BL2_CHESTSMANAGER",function()
 	c:EmitSound("player/object_use_stop_01.wav")
 	print("Found "..c:GetNWString("chest_type")..", setting up with item: "..c.Contents)
 	end
-
+	if c:GetModel() == "models/rena/borderlands/hyperion_weapon_box.mdl" and c:GetNWBool("isChest") == false then
+	c:SetNWBool("isChest",true)
+	c:SetNWBool("chest_open",false)
+	c:SetNWString("chest_type","shelf")
+	c.Contents = table.Random{"mg_wpn_f2000","item_ammo_ar2","mg_wpn_fnp","mg_wpn_tavor","mg_wpn_ump45","mg_wpn_m240b","mg_wpn_makarov","mg_wpn_mateba","mg_wpn_m9","mg_wpn_masada","mg_wpn_mp40","item_ammo_smg1","item_ammo_pistol","item_box_buckshot","item_ammo_357","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil","nil"}
+	c.moveBone = {[1] = {bone = "Hinge",
+				  		 ang = Angle(0,-90,0),
+				  		 pos = Vector(0,0,0),
+				  		 lerpang = Angle(0,0,0),
+				  		 angspeed = .015,
+				  		 lerppos = Vector(0,0,0),
+				  		 posspeed = 0.1,
+				  		},[5] = {bone = "Shelf",
+				  		 ang = Angle(0,0,0),
+				  		 pos = Vector(0,3,0),
+				  		 lerpang = Angle(0,0,0),
+				  		 angspeed = 0,
+				  		 lerppos = Vector(0,0,0),
+				  		 posspeed = 0.05,
+				  		},
+				  		[2] = {bone = "Arms",
+				  		 ang = Angle(0,0,0),
+				  		 pos = Vector(13,-7,0),
+				  		 lerpang = Angle(0,0,0),
+				  		 angspeed = 0.001,
+				  		 lerppos = Vector(0,0,0),
+				  		 posspeed = 0.01,
+				  		},
+				  		[3] = {bone = "L_Door",
+				  		 ang = Angle(0,-80,0),
+				  		 pos = Vector(-5,0,0),
+				  		 lerpang = Angle(0,0,0),
+				  		 angspeed = .03,
+				  		 lerppos = Vector(0,0,0),
+				  		 posspeed = 0.1,
+				  		},
+				  		[4] = {bone = "R_Door",
+				  		 ang = Angle(0,80,0),
+				  		 pos = Vector(-5,0,0),
+				  		 lerpang = Angle(0,0,0),
+				  		 angspeed = .03,
+				  		 lerppos = Vector(0,0,0),
+				  		 posspeed = 0.1,
+				  		},
+			
+				  }
+	c.angLerp = Angle(0,0,0)
+	c:EmitSound("player/object_use_stop_01.wav")
+	print("Found "..c:GetNWString("chest_type")..", setting up with item: "..c.Contents)
+	end
+	-------toilet
+	if c:GetModel() == "models/rena/borderlands/bandit_dookhut.mdl" and c:GetNWBool("isChest") == false then
+	c:SetNWBool("isChest",true)
+	c:SetNWBool("chest_open",false)
+	c:SetNWString("chest_type","toilet")
+	c.Contents = table.Random{"mg_wpn_f2000","mg_wpn_fnp","mg_wpn_tavor","mg_wpn_ump45","mg_wpn_m240b","mg_wpn_makarov","mg_wpn_mateba","mg_wpn_m9","mg_wpn_masada","mg_wpn_mp40"}
+	c.moveBone = {[1] = {bone = "door",
+				  		 ang = Angle(0,math.Rand(-20,40),0),
+				  		 pos = Vector(0,0,0),
+				  		 lerpang = Angle(0,-90,0),
+				  		 angspeed = .03,
+				  		 lerppos = Vector(0,0,0),
+				  		 posspeed = 0,
+				  		},
+				  		[2] = {bone = "Lid",
+				  		 ang = Angle(0,0,0),
+				  		 pos = Vector(0,0,0),
+				  		 lerpang = Angle(90,0,0),
+				  		 angspeed = .01,
+				  		 lerppos = Vector(0,0,0),
+				  		 posspeed = 0,
+				  		},
+			
+				  }
+	c.angLerp = Angle(0,-90,0)
+	c:EmitSound("player/object_use_stop_01.wav")
+	print("Found "..c:GetNWString("chest_type")..", setting up with item: "..c.Contents)
+	end
 	----LAUNDRY MACHINE
 	if c:GetModel() == "models/rena/borderlands/laundrymachine.mdl" and c:GetNWBool("isChest") == false then
 	c:SetNWBool("isChest",true)
@@ -198,11 +275,19 @@ hook.Add("Think","BL2_CHESTSMANAGER",function()
 	b.lerppos = LerpVector(b.posspeed,b.lerppos,b.pos)
 	c:ManipulateBonePosition(c:LookupBone(b.bone),b.lerppos)
 	end
+elseif c.moveBone then
+	for ind,b in pairs(c.moveBone) do
+		c:ManipulateBoneAngles(c:LookupBone(b.bone),LerpAngle(0,b.lerpang,b.ang))
+		c:ManipulateBonePosition(c:LookupBone(b.bone),LerpVector(0,b.lerppos,b.pos))
+	end
 	end
 
 	if c:GetNWBool("isChest") == true and c:GetNWBool("chest_open") == false and p:GetEyeTrace().Entity == c and c:GetPos():Distance(p:EyePos()) < 100 and p:KeyPressed(IN_USE) then
 	c:SetNWBool("chest_open",true)
 	c:EmitSound("doors/de_bank_doors_move_0"..math.random(1,3)..".wav")
+	if c:GetNWString("chest_type") == "shelf" then
+		c:EmitSound("doors/door_metal_thin_close2.wav")
+	end
 	if c.Contents != "nil" then
 	c.createdContent = ents.Create(tostring(c.Contents))
 	c.createdContent:SetPos(c:GetBonePosition(c:LookupBone(c.moveBone[1].bone)) + c:GetBoneMatrix(c:LookupBone(c.moveBone[1].bone)):GetAngles():Forward() * 15)
@@ -262,6 +347,16 @@ if CLIENT then
 	cam.Start3D2D(chest:GetPos() + chest:GetAngles():Up() * 16 + chest:GetAngles():Forward() * 14.2  + chest:GetAngles():Right() * -5,chest:GetAngles() + Angle(90,0,0),1)
 	surface.SetDrawColor(0,255,0,50)
 	surface.DrawRect(0,0,3,8)
+	cam.End3D2D()
+	elseif chest:GetNWString("chest_type") == "toilet" then
+	cam.Start3D2D(chest:GetPos() + chest:GetAngles():Up() * 81 + chest:GetAngles():Forward() * 8.5  + chest:GetAngles():Right() * -28.8,chest:GetAngles() + Angle(90,0,90),1)
+	surface.SetDrawColor(0,255,0,50)
+	surface.DrawRect(0,0,4,17)
+	cam.End3D2D()
+elseif chest:GetNWString("chest_type") == "shelf" then
+	cam.Start3D2D(chest:GetPos() + chest:GetAngles():Up() * 89.4 + chest:GetAngles():Forward() * -5.1  + chest:GetAngles():Right() * -24.4,chest:GetAngles() + Angle(90,0,90),1)
+	surface.SetDrawColor(0,255,0,50)
+	surface.DrawRect(0,0,7,4)
 	cam.End3D2D()
 	end
     end
